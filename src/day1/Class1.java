4317808756ec7e5beb4db1439835c741f49665c2
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-19 00:36:27 +0200

    Initial commit

---
+++ src/day1/Class1.java
@@ -0,0 +1,53 @@
+package day1;
+
+public class Class1 {
+    public static void main(String[] args) {
+        /*
+        System.out.println("Hello Ahiret");
+        System.out.println();
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+        System.out.println();
+        System.out.println("a\nb");
+        System.out.println("a\tb");
+        System.out.println("a\\b");
+        System.out.println("a\"b");
+         */
+        System.out.println("");
+       /*
+        String Anan="Senin anan";
+
+        System.out.println();
+
+        String Baban="Senin Baban";
+
+        System.out.println(Anan+Baban);
+
+        String Aile=Anan+Baban;
+
+        */
+
+        String Mercedes = "AMG";
+
+        String BMW = "M";
+
+        System.out.println(Mercedes + BMW);
+
+        String Car = Mercedes + BMW;
+
+        System.out.println(Car);
+
+        int a = 2;
+        System.out.println(10 % 2);
+
+        if (18%6  == 0) {
+            System.out.println("Number is even");
+        } else
+            System.out.println("Number is not even");
+
+    }
+}
